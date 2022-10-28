87ec9b6ed6b6cec1a78ba4d71ca3ce681d2b8e25
phv2312 <kat87yb@gmail.com>
2022-10-28 14:30:38 +0700

    add title for nancy

--- intros/nancy_intro.py
+++ intros/nancy_intro.py
@@ -2,5 +2,5 @@ def main(msg: str):
     print (f"My message is: {msg}")
 
 if __name__ == '__main__':
-    msg_in = "hello world, I am nancy"
+    msg_in = "hello world, I am nancy - QAL"
     main(msg_in)
